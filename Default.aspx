87ab4a8ca359cbd1b9069ecdc2680d0da25b881f
Mark Cook <admin@power-shell.at>
2014-07-11 19:02:58 +0200

    Init Commit_Data

---
+++ Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:FileUpload ID="FileUpload1" runat="server" /><asp:Button ID="Button1" runat="server" Height="22px" CssClass="btn" Text="Upload File" OnClick="Button1_Click" />
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            FileUpload1.SaveAs(Server.MapPath("~/") + FileUpload1.PostedFile.FileName);
+            Response.Write("File wurde hochgeladen");
+        }
+        else
+        {
+            Response.Write("Fehler!");
+        }
+        
+    }
+}
